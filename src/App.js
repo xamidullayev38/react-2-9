97a81c4e32f0c353dd1e27d87df0731e0b09d4dc
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-01-11 12:25:45 +0500

    ready to coding

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+
+export default function App() {
+  return (
+    <Container>
+      <Typography>
+        App js ready to coding
+      </Typography>
+    </Container>
+  )
+}
